e354cb294785cbd5f14933d0956da1a141345917
Alexander Marchand-Melsom <alexamar@stud.ntnu.no>
2020-10-27 12:04:03 +0100

    Add test edit file

--- TestEdit.java
+++ TestEdit.java
@@ -1,7 +1,7 @@
 package com.bytesvc;
 
 public class ServiceException extends Exception {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 599L;
 
 	public ServiceException(String message) {
 		super(message);
